912f69a807413ee64760c9b557db8ec916c9022d
Maksym Osinskyi <maximosinskiy@gmail.com>
2026-01-04 15:40:46 +0200

    feat: Remove data fields in OkResponse and BadResponse

--- backend/src/fitlife/schemas.py
+++ backend/src/fitlife/schemas.py
@@ -4,10 +4,8 @@ from pydantic import BaseModel
 class OkResponse(BaseModel):
     success: bool = True
     msg: str
-    data: BaseModel | None = None
 
 
 class BadResponse(BaseModel):
     success: bool = False
     msg: str
-    data: BaseModel | None = None
