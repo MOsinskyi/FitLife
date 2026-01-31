654f6103e93a83ac5b33d78f85ec7edc462298e9
Maksym Osinskyi <maximosinskiy@gmail.com>
2026-01-31 20:26:20 +0200

    feat(models): add phone_number field to CoachModel
    Added the phone_number field to the CoachModel to
    store contact information for each coach. This
    enhancement improves the functionality of the
    application by allowing users to manage and access
    coaches' phone numbers.

--- backend/src/fitlife/coach/models.py
+++ backend/src/fitlife/coach/models.py
@@ -6,7 +6,22 @@ from fitlife.models import Base
 class CoachModel(Base):
     __tablename__ = 'coaches'
 
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    email = Column(String, nullable=False)
-    password_hash = Column(String, nullable=False)
+    first_name = Column(
+        String,
+        nullable=False,
+    )
+    last_name = Column(
+        String,
+        nullable=False,
+    )
+    phone_number = Column(
+        String,
+        nullable=False,
+    )
+    email = Column(
+        String,
+    )
+    password_hash = Column(
+        String,
+        nullable=False,
+    )
