4c390343d59ed1e9c9ca641bb27382a161b00b50
Maksym Osinskyi <maximosinskiy@gmail.com>
2026-02-01 19:54:03 +0200

    refactor(migrations): restructure migration scripts
    Reorganized the migration scripts to improve clarity and
    maintainability. This includes creating a consistent structure
    for initial and subsequent migrations, making it easier
    to track changes and manage the database schema over time.
    All migration files are now properly aligned with Alembic
    standards, enhancing the overall workflow for database
    migrations.
    
    No issue references.

---
+++ backend/migrations/env.py
@@ -0,0 +1,71 @@
+import asyncio
+import sys
+from logging.config import fileConfig
+from os.path import abspath, dirname, join
+
+from alembic import context
+from sqlalchemy import Connection, pool
+from sqlalchemy.ext.asyncio import async_engine_from_config
+
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+backend_root = dirname(dirname(abspath(__file__)))
+sys.path.insert(0, join(backend_root, 'src'))
+
+from fitlife.coach.models import CoachModel  # noqa
+from fitlife.config import settings  # noqa
+from fitlife.member.models import MemberModel  # noqa
+from fitlife.models import Base  # noqa
+
+target_metadata = Base.metadata
+
+config.set_main_option('sqlalchemy.url', settings.database.url)
+
+
+def run_migrations_offline() -> None:
+    url = config.get_main_option('sqlalchemy.url')
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={'paramstyle': 'named'},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations() -> None:
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+def run_migrations_online() -> None:
+    asyncio.run(run_async_migrations())
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
