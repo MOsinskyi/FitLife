01ebe114773ab21388d9f20a030d498b980d4c26
Maksym Osinskyi <maximosinskiy@gmail.com>
2026-02-01 19:49:51 +0200

    refactor(cache): move custom key builder to utils module
    Refactored the `custom_key_builder` function by moving it from
    the `cache.py` file to a new `utils.py` module. This change
    improves code organization and separation of concerns, making
    the cache-related functions easier to manage and reuse across
    the application.
    
    No issue references.

--- backend/src/fitlife/cache.py
+++ backend/src/fitlife/cache.py
@@ -1,37 +1,9 @@
-import hashlib
-from collections.abc import Callable
-from typing import Any
-
 from fastapi import BackgroundTasks
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis.asyncio import Redis
-from starlette.requests import Request
-from starlette.responses import Response
 
 from fitlife.config import settings
-from fitlife.member.services import MemberService
-
-
-def custom_key_builder(  # noqa: PLR0913
-    func: Callable[..., Any],
-    namespace: str,
-    *,
-    request: Request | None = None,
-    response: Response | None = None,
-    args: tuple[Any, ...],
-    kwargs: dict[str, Any],
-) -> str:
-    exclude_types = (MemberService,)
-    new_kw = {}
-    for key, value in kwargs.items():
-        if isinstance(value, exclude_types):
-            continue
-        new_kw[key] = value
-    cache_key = hashlib.md5(  # noqa: S324
-        f'{func.__module__}:{func.__name__}:{args}:{new_kw}'.encode()
-    ).hexdigest()
-    return f'{namespace}:{cache_key}'
 
 
 async def init_cache():
---
+++ backend/src/fitlife/utils.py
@@ -0,0 +1,29 @@
+import hashlib
+from collections.abc import Callable
+from typing import Any
+
+from starlette.requests import Request
+from starlette.responses import Response
+
+from fitlife.member.services import MemberService
+
+
+def custom_key_builder(  # noqa: PLR0913
+    func: Callable[..., Any],
+    namespace: str,
+    *,
+    request: Request | None = None,
+    response: Response | None = None,
+    args: tuple[Any, ...],
+    kwargs: dict[str, Any],
+) -> str:
+    exclude_types = (MemberService,)
+    new_kw = {}
+    for key, value in kwargs.items():
+        if isinstance(value, exclude_types):
+            continue
+        new_kw[key] = value
+    cache_key = hashlib.md5(  # noqa: S324
+        f'{func.__module__}:{func.__name__}:{args}:{new_kw}'.encode()
+    ).hexdigest()
+    return f'{namespace}:{cache_key}'
