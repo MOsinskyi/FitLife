35d6c25ed85849920eaacdcd74fbc9f98a3b908f
Maksym Osinskyi <maximosinskiy@gmail.com>
2026-03-16 14:43:15 +0200

    ✨ feat: added `CoachSchema` as response model for coach endpoints

--- backend/src/fitlife/coach/routers.py
+++ backend/src/fitlife/coach/routers.py
@@ -5,8 +5,9 @@ from fastapi_cache.decorator import cache
 
 from fitlife.auth.dependencies import CurrentCoachDep
 from fitlife.coach.dependencies import CoachServiceDep
+from fitlife.coach.schemas import CoachSchema
 from fitlife.config import settings
-from fitlife.schemas import BadResponse, OkResponse, UserRegisterSchema, UserSchema
+from fitlife.schemas import BadResponse, OkResponse, UserRegisterSchema
 from fitlife.utils import custom_key_builder
 
 router = APIRouter()
@@ -19,10 +20,10 @@ title = '🏋️ Coaches'
     summary='Get all coaches',
     tags=[title],
     description='Get all coaches from the database. Any authenticated user.',
-    response_model=list[UserSchema],
+    response_model=list[CoachSchema],
     responses={
         status.HTTP_200_OK: {
-            'model': list[UserSchema],
+            'model': list[CoachSchema],
             'description': 'Coaches successfully retrieved.',
         },
         status.HTTP_400_BAD_REQUEST: {
@@ -43,11 +44,12 @@ async def get_coaches(service: CoachServiceDep):
 @router.get(
     '/coaches/{coach_uuid}',
     summary='Get specific coach by id',
+    response_model=CoachSchema,
     tags=[title],
     description='Get specific coach from database. Any authenticated user.',
     responses={
         status.HTTP_200_OK: {
-            'model': OkResponse,
+            'model': CoachSchema,
             'description': 'Coach successfully retrieved.',
         },
         status.HTTP_404_NOT_FOUND: {
