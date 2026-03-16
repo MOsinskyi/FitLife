09e8e1375de44f8be2b515ef2fed560cd0e0a52c
Maksym Osinskyi <maximosinskiy@gmail.com>
2026-03-16 09:22:40 +0200

    ✨ feat: added UserRegisterWithRoleSchema for role management in services

--- backend/src/fitlife/coach/services.py
+++ backend/src/fitlife/coach/services.py
@@ -2,7 +2,7 @@ from fastapi import BackgroundTasks
 
 from fitlife.coach.repositories import CoachRepository
 from fitlife.config import settings
-from fitlife.schemas import UserRegisterSchema
+from fitlife.schemas import UserRegisterSchema, UserRegisterWithRoleSchema
 from fitlife.security import SecurityDep
 from fitlife.services import UserService
 
@@ -19,6 +19,6 @@ class CoachService(UserService):
     async def create_coach(self, data: UserRegisterSchema):
         data_dict = data.model_dump()
         data_dict['role'] = 'coach'
-        data = UserRegisterSchema(**data_dict)
+        data = UserRegisterWithRoleSchema(**data_dict)
 
         return await super().create_user(data, self.security)
