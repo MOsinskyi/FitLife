69edcbeaba88178ff1d6b239fcf9b829754a0f13
Maksym Osinskyi <maximosinskiy@gmail.com>
2026-03-16 13:32:19 +0200

    ✨ feat: added `speciality` and `emoji_entity` columns to `CoachModel`

--- backend/src/fitlife/coach/models.py
+++ backend/src/fitlife/coach/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 from fitlife.models import UserBase
@@ -5,6 +6,22 @@ from fitlife.models import UserBase
 
 class CoachModel(UserBase):
     __tablename__ = 'coaches'
+
+    speciality = Column(
+        String,
+        nullable=False,
+        server_default='Силові тренування',
+        name='Спеціальність',
+        comment='Силові тренування, Функціональний фітнес, Йога та стретчинг, Кардіо та HIIT',
+    )
+
+    emoji_entity = Column(
+        String,
+        nullable=False,
+        server_default='&#x1F3CB;',
+        name='Emoji',
+        comment='HTML entity (hex)',
+    )
 
     sessions = relationship(
         'TrainingSessionModel',
