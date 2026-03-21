c52ab70ce7610c3127acef5405984c04773107aa
Maksym Osinskyi <maximosinskiy@gmail.com>
2026-03-21 14:58:17 +0200

    ✨ feat(db): add timestamps for created_at and updated_on fields
    Added `created_at` and `updated_on` fields to the Base model for
    automatically tracking record creation and updates. This enhances
    data integrity and auditing capabilities.
    
    Closes #123

--- backend/src/fitlife/models.py
+++ backend/src/fitlife/models.py
@@ -1,14 +1,36 @@
+from datetime import UTC, datetime
 from uuid import UUID, uuid4
 
 from sqlalchemy import Column, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
-class Base(DeclarativeBase):
-    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+class Base(
+    DeclarativeBase,
+):
+    id: Mapped[UUID] = mapped_column(
+        primary_key=True,
+        default=uuid4,
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        default=datetime.now(
+            UTC,
+        ),
+    )
+    updated_on: Mapped[datetime] = mapped_column(
+        default=datetime.now(
+            UTC,
+        ),
+        onupdate=datetime.now(
+            UTC,
+        ),
+    )
 
 
-class UserBase(Base):
+class UserBase(
+    Base,
+):
     __abstract__ = True
 
     first_name = Column(
@@ -27,7 +49,13 @@ class UserBase(Base):
         String,
     )
     password = Column(
-        String(255),
+        String(
+            255,
+        ),
         nullable=False,
     )
-    role = Column(String, nullable=False, default='member')
+    role = Column(
+        String,
+        nullable=False,
+        default='member',
+    )
