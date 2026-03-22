32103eb9b98d5fab4bf18a1cf692bd0582a42818
Maksym Osinskyi <maximosinskiy@gmail.com>
2026-03-22 14:21:05 +0200

    ✨ feat(auth): enhance authentication to support coaches as users
    This update introduces the ability to authenticate both members and
    coaches by adding a new repository for coaches. Users can now log in
    using their phone number regardless of their role, improving the
    flexibility of the authentication process.

--- backend/src/fitlife/auth/dependencies.py
+++ backend/src/fitlife/auth/dependencies.py
@@ -10,6 +10,8 @@ from fitlife.members.dependencies import MemberRepositoryDep
 from fitlife.models import UserBase
 from fitlife.security import SecurityDep
 
+from ..coaches.dependencies import CoachRepositoryDep
+from ..schemas import UserRoles
 from .services import AuthService
 
 if TYPE_CHECKING:
@@ -20,13 +22,15 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login')
 
 async def get_authentication_service(
     member_repository: MemberRepositoryDep,
+    coach_repository: CoachRepositoryDep,
     security: SecurityDep,
 ):
-    return AuthService(member_repository, security)
+    return AuthService(member_repository, coach_repository, security)
 
 
 async def get_current_user(
     member_repository: MemberRepositoryDep,
+    coach_repository: CoachRepositoryDep,
     security: SecurityDep,
     token: str = Depends(oauth2_scheme),
 ) -> 'UserBase':
@@ -41,8 +45,10 @@ async def get_current_user(
     except InvalidTokenError:
         raise InvalidCredentialsException('Invalid token') from None
 
-    if role == 'member':
+    if role == UserRoles.MEMBER.value:
         user = await member_repository.get_user_by_id(UUID(user_id))
+    elif role == UserRoles.COACH.value:
+        user = await coach_repository.get_user_by_id(UUID(user_id))
     else:
         raise InvalidCredentialsException('Invalid role')
 
--- backend/src/fitlife/auth/services.py
+++ backend/src/fitlife/auth/services.py
@@ -5,17 +5,27 @@ from fitlife.exceptions import InvalidCredentialsException
 from .schemas import TokenSchema
 
 if TYPE_CHECKING:
+    from fitlife.coaches.repositories import CoachRepository
     from fitlife.members.repositories import MemberRepository
     from fitlife.security import Security
 
 
 class AuthService:
-    def __init__(self, member_repository: 'MemberRepository', security: 'Security'):
+    def __init__(
+        self,
+        member_repository: 'MemberRepository',
+        coach_repository: 'CoachRepository',
+        security: 'Security',
+    ):
         self.member_repository = member_repository
+        self.coach_repository = coach_repository
         self.security = security
 
     async def authenticate_user(self, phone_number: str, password: str) -> TokenSchema:
         user = await self.member_repository.get_user_by_phone(phone_number)
+
+        if not user:
+            user = await self.coach_repository.get_user_by_phone(phone_number)
 
         if not user:
             raise ValueError('User not found')
