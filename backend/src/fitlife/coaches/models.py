91bc7a39d921e77f4b26b982859bc0f462fe17a6
Maksym <maximosinskiy@gmail.com>
2026-05-09 18:34:20 +0300

    feat(coaches): Update CoachModel to use relationship for specializations (main)
    This commit modifies the `CoachModel` to replace the previous
    array of strings for specializations with a relationship
    to the `SpecializationModel`. The update enhances data
    structure by allowing multiple specializations to be
    associated with each coach through a more efficient
    relationship mapping. This change improves the
    flexibility and usability of the coach data model.

--- backend/src/fitlife/coaches/models.py
+++ backend/src/fitlife/coaches/models.py
@@ -1,19 +1,14 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import Mapped, relationship
 
 from fitlife.models import UserBase
+from fitlife.specializations.models import coach_specialization_table, SpecializationModel
 from fitlife.training_sessions.models import TrainingSession
 
 
 class CoachModel(UserBase):
     __tablename__ = 'coaches'
 
-    specializations = Column(
-        ARRAY(String),
-        nullable=False,
-        default=list,
-    )
     emoji = Column(
         String,
         nullable=False,
@@ -30,6 +25,11 @@ class CoachModel(UserBase):
         default='',
     )
 
+    specializations: Mapped[list['SpecializationModel']] = relationship(
+        secondary=coach_specialization_table,
+        lazy='selectin',
+    )
+    
     sessions: Mapped[list['TrainingSession']] = relationship(back_populates='coach')
 
     def __str__(self):
