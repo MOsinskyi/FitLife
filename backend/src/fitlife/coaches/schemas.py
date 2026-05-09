01ce947ae3befb3ef952962c74b40829f1f858fd
Maksym <maximosinskiy@gmail.com>
2026-05-09 18:34:29 +0300

    feat(coaches): Refactor CoachModel to use UUIDs for specializations (main)
    This commit updates the `CoachModel` to replace the previous enum-based
    specializations with a list of UUIDs that reference the `SpecializationModel`.
    The new structure improves data integrity and allows for a more flexible
    association of multiple specializations per coach. The change enhances the
    overall usability and efficiency of the coach data model.

--- backend/src/fitlife/coaches/schemas.py
+++ backend/src/fitlife/coaches/schemas.py
@@ -1,32 +1,12 @@
-import enum
 from typing import Annotated
-
 from pydantic import ConfigDict, Field
-
 from fitlife import schemas as base
-
-
-class Specializations(enum.Enum):
-    YOGA = 'Yoga'
-    CROSSFIT = 'CrossFit'
-    AEROBICS = 'Aerobics'
-    STRENGTH_TRAINING = 'Strength Training'
-    CARDIO = 'Cardio'
-    MEDITATION = 'Meditation'
-
-
-class Emojis(enum.Enum):
-    YOGA = '🧘'
-    CROSSFIT = '🏋️'
-    AEROBICS = '🤸'
-    STRENGTH_TRAINING = '💪'
-    CARDIO = '🏃'
-    MEDITATION = '🪷'
-
+from fitlife.specializations.schemas import SpecializationResponse
+from uuid import UUID
 
 class CoachCreateSchema(base.UserCreateSchema):
-    specializations: Annotated[list[Specializations], Field(default_factory=lambda: [Specializations.STRENGTH_TRAINING])]
-    emoji: Annotated[Emojis, Field(default=Emojis.STRENGTH_TRAINING)]
+    specialization_ids: list[UUID] = Field(default_factory=list)
+    emoji: str = Field(default='💪')
     experience: Annotated[int, Field(default=1, ge=1)]
     experience_label: Annotated[str, Field(default='рік')]
 
@@ -38,11 +18,17 @@ class CoachRegisterSchema(base.UserRegisterSchema, CoachCreateSchema):
 
 
 class CoachUpdateSchema(base.UserUpdateSchema):
-    specializations: list[Specializations] | None = None
-    emoji: Emojis | None = None
+    specialization_ids: list[UUID] | None = None
+    emoji: str | None = None
     experience: int | None = None
     experience_label: str | None = None
 
 
-class CoachSchema(base.UserSchema, CoachCreateSchema):
+class CoachSchema(base.UserSchema):
+    specializations: list[SpecializationResponse] = Field(default_factory=list)
+    emoji: str
+    experience: int
+    experience_label: str
     role: str = 'coach'
+    
+    model_config = ConfigDict(from_attributes=True)
