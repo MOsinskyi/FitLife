bc3db067de0f6f62aef922ef06864533a9bf1d5c
Maksym <maximosinskiy@gmail.com>
2026-05-12 15:10:45 +0300

    feat(main): Update training session schema to use datetime for time fields (main)
    This commit modifies the training session schema to change the
    types of `start_time` and `end_time` from `time` to `datetime`.
    This update allows for better handling of time zones and enhances
    the accuracy of session scheduling. The default values are set
    to the current time in UTC, improving the overall functionality
    of the application.

--- backend/src/fitlife/training_sessions/schemas.py
+++ backend/src/fitlife/training_sessions/schemas.py
@@ -31,8 +31,8 @@ class TrainingSessionCreateSchema(BaseModel):
     description: Annotated[
         str, Field(default="Інтенсивне кругове тренування на всі групи м'язів. Підходить для новачків.")
     ]
-    start_time: Annotated[time, Field(default='10:00')]
-    end_time: Annotated[time, Field(default='11:00')]
+    start_time: Annotated[datetime, Field(default=datetime.now(UTC))]
+    end_time: Annotated[datetime, Field(default=datetime.now(UTC))]
     status: Annotated[SessionStatus, Field(default=SessionStatus.SCHEDULED)]
     max_participants: Annotated[int, Field(default=12, ge=1, le=12)]
     coach: UUID
